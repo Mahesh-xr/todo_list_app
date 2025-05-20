aee2d92111bb00b12511c9b266fe1a9970b97e0e
Mahesh-xr <mahesh.572xr@gmail.com>
2025-05-20 23:42:31 +0530

    add listView

--- lib/main.dart
+++ lib/main.dart
@@ -17,33 +17,63 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body:Column(
+        appBar: AppBar(
+          title: Text("To do List"),
+          backgroundColor: Colors.blue,
+          centerTitle: false,
+        ),
+
+        body: Column(
           children: [
-            Container(
-              padding: EdgeInsets.all(10),
-              child: TextField(
-                controller: textEditingController,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: "Enter a Task"
+            Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    padding: EdgeInsets.all(10),
+                    child: TextField(
+                      controller: textEditingController,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: "Enter a Task",
+                      ),
+                    ),
+                  ),
+                ),
+                MaterialButton(
+                  color: Colors.white,
+                  height: 50,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(50),
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      task = textEditingController.text;
+                    });
+                  },
+                  child: Text("click"),
+                ),
+              ],
+            ),
+          Row(
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Text(task),
                 ),
               ),
-            ),
-            MaterialButton(
-            color: Colors.white,
-            height: 50,
-            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(50)),
-            onPressed: (){
-              setState(() {
-                task = textEditingController.text;
-              });
-            },
-            child: Text("click"),),
-            Text(task)
+              MaterialButton(onPressed: (){},
+              child: Icon(Icons.delete, color: Colors.red,),
+              )
+            ],
+          ),
+          ListView.builder(itemBuilder: (context, index){
+            return 
+          })
+
           ],
         ),
       ),
